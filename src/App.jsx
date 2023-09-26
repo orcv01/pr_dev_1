17f8135d8098d386bb823a7d7a6adfd880766615
UsuarioM <orcv01@gmail.com>
2023-09-26 14:03:32 +0200

    Upload pr_dev_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const charactersMock = [
+{
+id: 1,
+name: "Rick Sanchez",
+status: "Alive",
+},
+{
+id: 2,
+name: "Morty Smith",
+status: "Alive",
+},
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+const App = () => {
+
+  const [myState, setMySatate] = useState ([charactersMock]);
+  //const [characterList, setCharacterList] = React.useState(charactersMock);
+  const [characterList, setCharacterList] = useState([charactersMock]);
+
+  const characters = myState[0];
+  const charactersSet = myState[1];
+
+  
+
+return 
+<h1>Hello React!</h1>;
+
+<div>
+  {characterList.map((character) => (
+  <p>{character.name}</p>
+  ))}
+</div>
+
+};
+
+export default App;
